0c5b250f6bdb356714c9ea5eb8958741d7a939c4
Leonardo Ferreira <leo.oliveira.eng@outlook.com>
2021-09-17 07:22:07 -0300

    #10 - Implementing identity context

---
+++ IdentityServer.Data/Context/IdentityContext.cs
@@ -0,0 +1,61 @@
+﻿using BaseEntity.Domain.Entities;
+using IdentityServer.Data.Context.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Data.Context
+{
+    public class IdentityContext : DbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> dbContextOptions)
+            : base(dbContextOptions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+
+            foreach (var type in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(Entity).IsAssignableFrom(type.ClrType) && (type.BaseType == null || !typeof(Entity).IsAssignableFrom(type.BaseType.ClrType)))
+                    modelBuilder.SetSoftDeleteFilter(type.ClrType);
+            }
+        }
+
+        public override int SaveChanges()
+        {
+            UpdateSoftDeleteStatuses();
+
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            UpdateSoftDeleteStatuses();
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void UpdateSoftDeleteStatuses()
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (!(entry.Entity is Entity))
+                    continue;
+
+                switch (entry.State)
+                {
+                    case EntityState.Deleted:
+                        (entry.Entity as Entity).Delete();
+                        entry.State = EntityState.Modified;
+                        break;
+                }
+            }
+        }
+    }
+}
