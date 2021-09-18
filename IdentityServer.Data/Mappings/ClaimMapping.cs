c454e403dcf9c3e8fccce6afaf1265b0168fbba0
Leonardo Ferreira <leo.oliveira.eng@outlook.com>
2021-09-17 21:09:21 -0300

    #10 - mapping entities for ef

---
+++ IdentityServer.Data/Mappings/ClaimMapping.cs
@@ -0,0 +1,18 @@
+﻿using IdentityServer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdentityServer.Data.Mappings
+{
+    public class ClaimMapping : IEntityTypeConfiguration<Claim>
+    {
+        public void Configure(EntityTypeBuilder<Claim> builder)
+        {
+            builder.ToTable(nameof(Claim));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Ignore(x => x.DomainEvents);
+        }
+    }
+}
