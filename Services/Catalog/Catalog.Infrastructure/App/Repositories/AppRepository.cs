01f8aaa8ec466fc73df6b94a40df49c9c6f7369a
Leonardo Ferreira <leo.oliveira.eng@outlook.com>
2021-10-10 13:12:16 -0300

    #20 - implementing app repository

--- Services/Catalog/Catalog.Infrastructure/App/Repositories/AppRepository.cs
+++ Services/Catalog/Catalog.Infrastructure/App/Repositories/AppRepository.cs
@@ -1,12 +1,14 @@
-﻿using Catalog.Common.Configurations;
+﻿using BaseEntity.Domain.Mediator.Contracts;
 using Catalog.Domain.Apps.Repositories;
-using Catalog.Infrastructure.Shared;
-using Microsoft.Extensions.Options;
+using Catalog.Infrastructure.Context.Contracts;
+using Catalog.Infrastructure.Shared.Repositories;
+using Model = Catalog.Domain.Apps.Models;
 
 namespace Catalog.Infrastructure.App.Repositories
 {
-    public class AppRepository //: MongoDbBaseRepository, IAppRepository
+    public class AppRepository : BaseRepository<Model.App>, IAppRepository
     {
-        //public AppRepository(IOptions<MongoDbConfiguration> mongoDbConfiguration) : base(mongoDbConfiguration) { }
+        public AppRepository(IMongoContext context, IMediatorHandler mediatorHandler)
+            : base(context, mediatorHandler) { }
     }
 }
