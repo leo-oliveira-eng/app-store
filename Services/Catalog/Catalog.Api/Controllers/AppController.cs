e6d7b05474213098f798f9ad78f64e648a0a7329
Leonardo Ferreira <leo.oliveira.eng@outlook.com>
2021-10-12 23:32:13 -0300

    #20 - implementing app controller

---
+++ Services/Catalog/Catalog.Api/Controllers/AppController.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Application.Apps.Contracts;
+using Catalog.Messages.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Controller = Catalog.Api.Controllers.Default.Controller;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class AppController : Controller
+    {
+        private IAppApplicationService AppApplicationService { get; }
+
+        public AppController(IAppApplicationService appApplicationService)
+        {
+            AppApplicationService = appApplicationService ?? throw new ArgumentNullException(nameof(appApplicationService));
+        }
+
+        [HttpPost, Route("")]
+        public async Task<IActionResult> CreateAsync([FromBody] CreateAppRequestMessage requestMessage)
+            => await WithResponseAsync(() => AppApplicationService.CreateAsync(requestMessage));
+    }
+}
